b4ad388060bc9f96efc50089350923f714d9c9ff
shupengsun <sp_sun@163.com>
2018-11-15 15:25:10 +0800

    Create main.go
    Add myslq code

---
+++ mysql/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "base:1qazxsw2@tcp(192.168.162.108:3306)/alarm")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err.Error())
+	}
+	stmt, err := db.Prepare("SELECT app_id,name FROM alarm_app WHERE id=?")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer stmt.Close()
+
+	var app_id, name string
+	err = stmt.QueryRow(21).Scan(&app_id, &name)
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Printf("result: %s , %s", app_id, name)
+
+}
